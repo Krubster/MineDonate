53ab290779065d6680132759ae3037f0abc79e2d
1061911 <log_inil@log_inil.ru>
2017-09-02 12:49:22 +0300

    0.7.1.23#addition3

--- forge 1.7.10/src/main/java/ru/alastar/minedonate/events/PlayerJoinEventHandler.java
+++ forge 1.7.10/src/main/java/ru/alastar/minedonate/events/PlayerJoinEventHandler.java
@@ -1,8 +1,8 @@
 package ru.alastar.minedonate.events;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import ru.alastar.minedonate.MineDonate;
 import ru.alastar.minedonate.network.packets.CodePacket;
 import ru.alastar.minedonate.rtnl.ModNetworkRegistry;
@@ -12,15 +12,10 @@ import ru.alastar.minedonate.rtnl.ModNetworkRegistry;
  * */
 public class PlayerJoinEventHandler {
 
-   @SubscribeEvent
-   public void onEntityJoin ( EntityJoinWorldEvent event ) {
-
-       if ( ! event . world . isRemote && event . entity instanceof EntityPlayerMP ) {
-           
-    	  ModNetworkRegistry . sendTo ( ( EntityPlayerMP ) event . entity, new CodePacket ( MineDonate . m_Enabled ? CodePacket . Code . MOD_ENABLED : CodePacket . Code . MOD_DISABLED ) ) ;
-           
-       }
-       
-   }
+	@SubscribeEvent
+	public void onEntityJoin ( PlayerEvent . PlayerLoggedInEvent event ) {
+   
+		ModNetworkRegistry . sendTo ( ( EntityPlayerMP ) event . player, new CodePacket ( MineDonate . m_Enabled ? CodePacket . Code . MOD_ENABLED : CodePacket . Code . MOD_DISABLED ) ) ;
+	}
 	   
 }
